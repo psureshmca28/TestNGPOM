bdfe6676e861e66a53fb63c6f4aa8393f30b0dd5
psureshmca28 <115546208+psureshmca28@users.noreply.github.com>
2022-10-12 11:31:28 +0530

    Create Test1.java

---
+++ src/test/java/Test1.java
@@ -0,0 +1,22 @@
+
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	
+	  @Test
+	  public void TestCase_1() {
+		  System.out.println("Test1 one suresh rrrrrrrrrrrr");
+	  }
+  
+	  @Test
+	  public void TestCase_2() {
+		  System.out.println("Test1 two suresh yyyyyyyyyyyyyyyyy");
+	  }
+	  
+	  
+	 
+  
+	
+}
