4a99e1268d977a704c7b39dcedd03fd310b398f1
Suresh Pelluru <Suresh Pelluru@DESKTOP-B4ND95H>
2022-10-12 11:15:37 +0530

    testng first commit

---
+++ src/test/java/Test2.java
@@ -0,0 +1,22 @@
+
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	
+	  @Test
+	  public void TestCase_1() {
+		  System.out.println("Test2 one suresh MMMMMMMMMMM");
+	  }
+  
+	  @Test
+	  public void TestCase_2() {
+		  System.out.println("Test2 two suresh YYYYYYYYYYY");
+	  }
+	  
+	  
+	 
+  
+	
+}
